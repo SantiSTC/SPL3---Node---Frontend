e55cbc7a1d3cde7197cf0c3dd9d0654cb06e9b52
SantiSTC <santiagoiannellot@gmail.com>
2023-12-10 22:30:09 -0300

    Api

--- frontend/js/func.ts
+++ frontend/js/func.ts
@@ -1,2 +1,2 @@
 const URL_API : string = "http://localhost:2023/";
-const URL_BASE : string = "http://localhost/labo3/SPL3/frontend/";
+const URL_BASE : string = "http://localhost/labo3/SPL3/frontend/"; 
